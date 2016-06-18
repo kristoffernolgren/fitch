05f83c0ec512acb2ecf4eca12f86d1574d870294
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-06-18 20:20:02 +0200

    Remove backend fb-auth

--- index.js
+++ index.js
@@ -7,23 +7,6 @@ app.use(passport.initialize());
 
 app.get('/auth/facebook',  passport.authenticate('facebook',  { session: false }));
 
-app.get('/auth/facebook/callback',
-	passport.authenticate('facebook',
-	{
-		session: false,
-		successRedirect: '/auth/facebook/complete',
-		failureRedirect: '/auth/facebook/fail'
-	})
-);
-
-app.get('/auth/facebook/complete', (req, res) => {
-	res.send('FB Auth complete');
-});
-
-app.get('/auth/facebook/fail', (req, res) => {
-	res.send('FB Auth failed');
-});
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
--- passport.js
+++ passport.js
@@ -1,7 +1,6 @@
 var passport = require('passport'),
 	FacebookStrategy = require('passport-facebook').Strategy;
 	local = require('passport-local').Strategy,
-	facebook = require('passport-facebook').Strategy,
 	settings = require('./settings.js').settings;
 
 
@@ -9,18 +8,6 @@ passport.use(new local(
   function(username, password, done) {
       return done(null, {});
   }
-));
-
-passport.use(new FacebookStrategy(settings.facebookCredentials,
-	function(accessToken, refreshToken, profile, done) {
-		console.log('test')
-		console.log(accessToken, refreshToken, profile)
-	/*User.findOrCreate(..., function(err, user) {
-	  if (err) { return done(err); }
-	  done(null, user);
-	});*/
-	done(null, {});
-	}
 ));
 
 exports.passport = passport;
