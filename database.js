41d8424e1882f72ef6164ab5a10bfc4ecaecfeb3
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-06-19 20:13:42 +0200

    datamodel.

--- database.js
+++ database.js
@@ -7,18 +7,36 @@ var sequelize = new Sequelize('database', 'username', 'password', {
   storage: 'db.sqlite'
 });
 
-var User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE
+var Driver = sequelize.define('driver', {
+	auth:		Sequelize.STRING,
+	name:		Sequelize.STRING,
+	phone:		Sequelize.INTEGER,
+	bank:		Sequelize.INTEGER,
+	idNo:		Sequelize.STRING
 });
 
-sequelize.sync().then(function() {
-  return User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  });
-}).then(function(jane) {
-  console.log(jane.get({
+var Rider = sequelize.define('rider', {
+	auth:		Sequelize.STRING,
+	name:		Sequelize.STRING,
+	phone:		Sequelize.INTEGER,
+	payment:	Sequelize.STRING,
+});
+
+var Hail = sequelize.define('hail', {
+	lat:		Sequelize.INTEGER,
+	lon:		Sequelize.INTEGER,
+});
+
+Hail.belongsTo(Driver);
+Hail.belongsTo(Rider);
+
+sequelize.sync({force: true}).then(function() {
+	return Hail.create({
+		lat: 123,
+		lon: 1234
+	});
+}).then(function(driver) {
+  console.log(driver.get({
     plain: true
   }));
 });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	passport = require('./passport.js').passport;
 
-require('./database.js')
+require('./database.js');
 
 app.use(passport.initialize());
 app.use(express.static('public'));
