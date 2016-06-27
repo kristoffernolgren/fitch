0b51bad09a4708d1dbf6620e116bafa59ac57ea5
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-06-27 19:01:31 +0200

    routing.

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
 var app = require('../app.js').app,
 	passport = require('./passport.js').passport,
 	sequelize = require('../database.js').sequelize,
-	User = sequelize.models.user,
-	auth = passport.authenticate('facebook-token',  { session: false });
+	User = sequelize.models.user;
 
 
 app.use(passport.initialize());
 
-app.get('/user/me', auth,
-	(req, res) => {
-		res.json(req.user);
+app.all('*', passport.authenticate('facebook-token',  { session: false }));
+
+app.get('/user/me',
+	(req, res, next) => {
+		next();
 });
 
-app.get('/user/makerider', auth,
-	(req, res) => {
+app.get('/user/makeRider',
+	(req, res,next) => {
 		req.user.user.makeRider(req.query)
 			.then((user) => {
-				res.json(req.user);
+				next();
 			});
 	}
 );
+
+app.all('*', (req, res) => {
+	req.user.user.full().then((fulluser) => {
+		res.json({
+			user: fulluser,
+			params: req.query
+		});
+	});
+});
