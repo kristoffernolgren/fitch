d1fa224a2ce5a4f1a3361a17e81a2f2bfe8e9bed
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-06-28 16:20:35 +0200

    userAttributes validation

--- models/userAttributes.js
+++ models/userAttributes.js
@@ -1,9 +1,37 @@
 var sequelize = require('../database.js').sequelize,
 	Sequelize = require('../database.js').Sequelize,
+	validator = require('validator'),
+	tests = {
+		phone: {
+			test: (value) => validator.isInt(value),
+			msg: 'phonenumber must contain numbers'
+		}
+	};
+
 
 UserAttribute = sequelize.define('userAttributes', {
-	name:		Sequelize.STRING,
-	value:		Sequelize.STRING,
+	name: {
+		type:		Sequelize.STRING,
+		allowNull:	false,
+		validate: {
+			isIn: [['phone', 'name']],
+		}
+	},
+	value: {
+		type:		Sequelize.STRING,
+		allowNull:	false
+	},
+}, {
+	validate: {
+		InvalidValue: function() {
+			var test = tests[this.name],
+				value = this.value;
+			if(typeof test !== 'undefined' && !test.test(value)){
+				throw new Error(test.msg);
+			}
+
+		}
+	}
 });
 
 
