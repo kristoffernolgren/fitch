595c419b4a34ed1a0a09d2a78074939530937b3c
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-06-29 17:56:40 +0200

    initial validation.

--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,53 @@ var app = require('../app.js').app,
 	Sequelize = require('../database.js').Sequelize,
 	sequelize = require('../database.js').sequelize,
 	User = sequelize.models.user,
-	auth = passport.authenticate('facebook-token',  { session: false });
+	bodyParser = require('body-parser'),
+	validator = require('express-validator'),
+	auth = (req, res, next) => {
+		passport.authenticate('facebook-token',	(err, user, info) => {
+			req.assert("access_token", "requierd").notEmpty();
+			req.assert("access_token", "Invalid access_token").invalidAuth(err);
+			if (err || !user) {
+				return render(req, res);
+			}
+
+			req.logIn(user, { session: false }, (err) => next());
+		})(req, res, next);
+	},
+	render = (req, res) => {
+		var response = {};
+		if(req.user){
+			response.user = req.user.fulluser;
+		}
+		if(Object.keys(req.query).length){
+			response.params = req.query;
+		}
+		if(req.validationErrors().length > 0 ){
+			response.errors = req.validationErrors();
+		}
+		res.json(response);
+	};
 
 app.use(passport.initialize());
 
-app.use((req, res, next) => {
-	req.resolve = (promise) => {
-		return promise.then(() => {
-			return req.user.user.full().then((fulluser) => {
-				return res.json({
-					user: fulluser,
-					params: req.query
-				});
-			});
-		}).catch(Sequelize.ValidationError, err => {
-				// respond with validation errors
-				return res.status(422).send(err.errors);
-		}).catch(err => {
-			// every other error
-			return res.status(400).send({ message: err.message });
-		});
-	};
-	next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+	validator({
+		customValidators: {
+			invalidAuth: (value, err) => err === null
+		}
+	})
+);
 
 
-app.get('/user/me', auth,
+app.all('/user/me', auth,
 	(req, res, next) => {
-		req.resolve(Promise.resolve());
+		render(req, res);
 	});
 
 app.get('/user/makeRider', auth,
 	(req, res, next) => {
-		req.resolve(req.user.user.makeRider(req.query));
+		req.user.user.makeRider(req.query)
+			.then(render(req, res));
 	}
 );
-/*
-app.all('*', (req, res) => {
-
-});
-*/
--- routes/passport.js
+++ routes/passport.js
@@ -5,10 +5,24 @@ var passport = require('passport'),
 	sequelize = require('../database.js').sequelize,
 	User = sequelize.models.user;
 
+//should pass return done(err) if user.auth fails
 passport.use(new facebook(settings,
 	(accessToken, refreshToken, profile, done) => {
-		User.auth(profile.id).then((user) => done(null, {user: user, profile: profile}));
+		if(typeof profile === 'undefined'){
+			done(null, false, {message: 'invalid accessToken'});
+		}
+		User.auth(profile.id)
+			.then((user) => {
+				return user.full()
+					.then((fulluser) => {
+						return {user: user, fulluser:fulluser};
+					});
+			})
+			.then((user) => done(null, {fulluser: user.fulluser, user: user.user, profile: profile}));
 	}
 ));
+
+
+
 
 exports.passport = passport;
