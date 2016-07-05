7dc8bce214e790041ac6dd595760f2cc2e011c67
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-07-05 20:52:02 +0200

    hail validations

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@ app.get('/hail/create', auth, (req, res, next) => {
 			req.assert('lat', 'required').notEmpty(),
 			req.assert('lon', 'required').notEmpty(),
 			req.assert('user phone','required for making a hail').userHas('phone', req.user),
-			req.assert('lat', 'location not allowed').inside(q.lat, q.lon),
-			req.assert('lon', 'location not allowed').inside(q.lat, q.lon)
+			req.assert('lat', 'invalid location').inside(q.lat, q.lon),
+			req.assert('lon', 'invalid location').inside(q.lat, q.lon)
 		];
 
 	if(isValid(test)){
