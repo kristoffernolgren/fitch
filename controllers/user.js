66b3c6ad4928df93e7545b4e81c4a833d1572095
Kristoffer Nolgren <kirstoffer@nolgren.se>
2016-07-13 11:32:22 +0200

    cleanup

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,22 @@
 var	isValid =	require('./validator.js').isValid,
 	edit = (req, res, next) => {
 		var attributes = ['name', 'phone', 'bank', 'bankNo'],
+			q = req.query,
 			test;
+
+		//updated userdetails
 		attributes.forEach((attribute) => {
-			//Only do requests on lines that have properties.
-			if(Boolean( req.query[attribute] )){
+			if(Boolean( q[attribute] )){
 				test = req.assert(attribute, 'required').optional().notEmpty();
 				if(isValid(test)){
-					req.user.setAttribute(attribute, req.query[attribute]);
+					req.user.setAttribute(attribute, q[attribute]);
 				}
 			}
 		});
-		//driverRequest has special conditions
-		if(Boolean(req.query.driverrequest)){
+
+
+		//Make driver request
+		if(Boolean(q.driverrequest)){
 			test = [
 				req.assert('phone', 'required for becomming a driver').userHas('phone',req.user),
 				req.assert('bank', 'required for becomming a driver').userHas('bank',req.user),
@@ -22,10 +26,14 @@ var	isValid =	require('./validator.js').isValid,
 				req.user.setAttribute('driverRequest', 'true');
 			}
 		}
+
 		return next();
 	},
+
+
 	admin =	(req, res, next) => {
-		if(Boolean(req.query.driver)){
+		var q = req.query;
+		if(Boolean(q.driver)){
 			var test = [
 				req.assert('user', 'Must be admin').userHas('admin',req.user),
 				req.assert('User', 'Is already driver' ).userHasNot('driver', res.locals.targetUser)
